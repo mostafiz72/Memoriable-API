23ec5445fd63aaafbe5953aa16723e9a10c1d52e
Hera <mostafizhera@gmail.com>
2024-10-22 01:04:10 +0600

    new created sunglass and watch folder and file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <h2>amar sonar bangla ami tomai valo basi</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Watch/Notes.js
@@ -0,0 +1,7 @@
+/// amra kono function pass korle amara abar sei function ta k amara use korete pari butt kono function er nam jodi boro hoy ta hole amara 
+// as >>>>>>>>>>>>> Use korte pari
+
+// Example:- ***********************************
+
+// Function 1:- mone kori amara kothai theke function ta export korsi and sei function ta k import korsi 
+// example:- import {dividedTheFirstNumberByTheSecoundNumber as divided}===================== ta hole amara divided nam ta k function ta k niya kaj korte parbo.
